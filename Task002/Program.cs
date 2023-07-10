46ac53abe61cc15a97dc374fd983b0bdb21733aa
Misha <mnwahr@gmail.com>
2023-07-10 15:55:08 +0200

    New002

---
+++ Task002/Program.cs
@@ -0,0 +1,7 @@
+﻿//На ввод принимаем два числа и проверяем второе число квадратом первого
+Console.WriteLine("Введите первое число");
+string inputedString1 = Console.ReadLine();
+int number1 = Convert.ToInt32(inputedString1);
+Console.WriteLine("Введите второе число");
+string inputedString2 = Console.ReadLine();
+int number2 = Convert.ToInt32(inputedString2);
