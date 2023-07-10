08859e21664bd4f79095794d067ad3ce8d2c2d86
Misha <mnwahr@gmail.com>
2023-07-10 15:19:35 +0200

    SquareNumber

---
+++ Task001_squreNumber/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите програму, которая на взод принимает число
+// и выдает его квадрат (число умноженное на само себя)
+
+Console.Write("Введите число => "); //Вывод в консоль приглашения.
+string inputedString = Console.ReadLine ();//Ввод пользователя (строка).
+int number = Convert.ToInt32(inputedString);//Преоброазования строки в число. 
+Console.WriteLine($"Вы ввели число {number}!");//Вывод введенного значения.
+int square = number * number;//Логика программы. 
+Console.WriteLine($"Квадрат числа {number} равен {square}!");//Вывод результата.
