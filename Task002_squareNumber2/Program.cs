e953c3f18103912ba0a4b3fe0571fc24b60d4b30
Misha <mnwahr@gmail.com>
2023-07-11 17:38:42 +0200

    Finish002New Folder

---
+++ Task002_squareNumber2/Program.cs
@@ -0,0 +1,20 @@
+﻿//На ввод принимаем два числа и проверяем второе число квадратом первого
+Console.WriteLine("Введите первое число");
+string inputedString1 = Console.ReadLine();
+int number1 = Convert.ToInt32(inputedString1);
+Console.WriteLine("Введите второе число");
+string inputedString2 = Console.ReadLine();
+int number2 = Convert.ToInt32(inputedString2);
+//int square = number1 * number1;
+//if (square == number2)
+if(number2 == number1*number1)
+{
+    Console.WriteLine("Второе число является квадратом первого!");
+}
+    else 
+    {
+    Console.WriteLine("Второе число не является квадратом первого!");
+    }
+
+
+
