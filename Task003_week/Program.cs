49b2d06121571f9e5f4d3a9f72c8234f1461a4a1
Misha <mnwahr@gmail.com>
2023-07-13 15:29:06 +0200

    newupdate

--- Task003_week/Program.cs
+++ Task003_week/Program.cs
@@ -1,4 +1,17 @@
 ﻿//На ввод принимаем числа от 1-7, на вовод даем название дня недели
+Console.WriteLine("Введите число для поиска дня недели!");
+string [] week = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+string weekday = Console.ReadLine();
+int day = Convert.ToInt32(weekday);
+Console.WriteLine(week[day -1]);
+
+
+
+
+
+
+
+//На ввод принимаем числа от 1-7, на вовод даем название дня недели
 Console.WriteLine("Введите пожалуйста число!");
 string inputedString = Console.ReadLine();
 int day = Convert.ToInt32(inputedString);
