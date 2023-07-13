39308b00fe5f827de8cb17fa71ba5b9ace15581c
Misha <mnwahr@gmail.com>
2023-07-13 15:50:29 +0200

    NewWeek003

--- Task003_week/Program.cs
+++ Task003_week/Program.cs
@@ -1,6 +1,13 @@
 ﻿//На ввод принимаем числа от 1-7, на вовод даем название дня недели
 Console.WriteLine("Введите число для поиска дня недели!");
-string [] week = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+string[] week = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
 string weekday = Console.ReadLine();
 int day = Convert.ToInt32(weekday);
-Console.WriteLine(week[day -1]);
+if (day < 1 || day > 7)
+{
+    Console.WriteLine("Под таким порядковым номером дне не существует");
+}
+else
+{
+    Console.WriteLine(week[day - 1]);
+}
