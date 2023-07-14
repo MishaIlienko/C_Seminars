d8e96657e955f45e5940374fe00cc3ced7b98b55
Misha <mnwahr@gmail.com>
2023-07-14 17:08:31 +0200

    New_14.07

---
+++ Task004_X=F(a)/Program.cs
@@ -0,0 +1,6 @@
+﻿// Нпишите программу вычисления функции х = f(a)
+Console.WriteLine("Введите число!");
+string input = Console.ReadLine();
+int num = Convert.ToInt32 (input);
+int num1 = num * num;
+Console.WriteLine(num1+1);
---
+++ Task005_from-N_to_N/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает одно число (N), а на выходе 
+//показывает все целые числа в промежутке от -N до N.
+Console.WriteLine("Введите число");
+string input = Console.ReadLine();
+int num = Convert.ToInt32(input);
+int num1 = -num;
+while(num1 <= num)
+{
+    Console.Write(num1);
+    num1++;
+}
