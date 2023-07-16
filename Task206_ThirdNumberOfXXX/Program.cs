e1b9ebbdc70761434c7b4021c45afa676870f4c0
Misha <mnwahr@gmail.com>
2023-07-16 23:25:21 +0200

    206

--- Task206_ThirdNumberOfXXX/Program.cs
+++ Task206_ThirdNumberOfXXX/Program.cs
@@ -1,5 +1,24 @@
 ﻿// Напишите программу, которая принимает трехзначное число 
 // и на выходе показывает его последнюю цифру.
-Console.WriteLine("Введите трехзначное число!");
-string input = Console.ReadLine();
-int num = Convert.ToInt32(input);
+int ReadInt(string message)
+{
+    System.Console.WriteLine(message);
+    int input = Convert.ToInt32(Console.ReadLine());
+    return input;
+}
+bool Validate3sign(int number)
+{
+    if (number >= 100 && number < 1000)
+    {
+    return true;
+    }
+    System.Console.WriteLine("Число не трехзначное!");
+    return false;
+}
+int num = ReadInt("Введите число ->");
+if (Validate3sign(num))
+{
+        int lastDigit = num %10;
+        System.Console.WriteLine($"Последнее число {lastDigit}");
+    
+}
