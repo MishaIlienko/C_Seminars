10b68aeafbae45cf9b6b70477f1043a20a881603
Misha <mnwahr@gmail.com>
2023-07-23 18:01:14 +0200

    210

---
+++ Task210/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит случайное число [10, 9999]
+//и показывает наибольшую цифру
+
+int number = new Random().Next(10, 10000);
+int num1 = 0;
+int num2 = 0;
+System.Console.WriteLine($"Из числа {number} наибольшая цифра -> ");
+while (number > 0)
+{
+    num1 = number % 10;
+    if (num1 > num2)
+    {
+        num2 = num1;
+    }
+    number /= 10;
+}
+System.Console.WriteLine(num2);
---
+++ Task210_RandomFrom10To9999/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит случайное число [10, 9999]
+//и показывает наибольшую цифру
+
+int number = new Random().Next(10, 10000);
+int a = number % 10;
+int b = number / 10 % 10;
+int c = number / 100 % 10;
+int d = number / 1000;
+int max = 0;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+System.Console.WriteLine($"В числе {number} максимальная цифра {max}!");
